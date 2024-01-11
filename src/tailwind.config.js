ded6682f8cb1e7bfb388e2155f8a27153944ae59
Wang Khang Waei <devtesting404@gmail.com>
2024-01-11 16:27:23 +0800

    update: update npm stuff

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './website/*.{js,ts,php,html}',
-    './controller/**/*.{js,ts,php,html}',
-    './view/**/*.{js,ts,php,html}',
+    './controller/*.{js,ts,php,html}',
+    './view/pages/**/*.{js,ts,php,html}',
+    './view/js/**/*.{js,ts,php,html}',
   ],
   theme: {
     extend: {},

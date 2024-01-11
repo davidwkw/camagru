4fb368b31be72ae5c04599a1135819e70594d61b
Wang Khang Waei <devtesting404@gmail.com>
2024-01-11 16:27:48 +0800

    update: implement actual controller

--- src/controller/index.php
+++ src/controller/index.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Camagru</title>
-  <link rel="stylesheet" href="../view/css/global.css">
-</head>
-
-<body>
-  <?php include('../view/html/components/header.html') ?>
-  <main>
-    <p class="text-4xl text-green-500">Hello world</p>
-  </main>
-  <?php include('../view/html/components/footer.html') ?>
-</body>
-
-</html>
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  return include("../view/pages/home.php");
+};

69daff8353608fd64e8ef3d9c4e3c62f717d2c53
Wang Khang Waei <devtesting404@gmail.com>
2024-01-11 15:31:54 +0800

    update: rename website folder to controller folder

---
+++ src/controller/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Camagru</title>
+  <link rel="stylesheet" href="../view/css/global.css">
+</head>
+
+<body>
+  <?php include('../view/html/components/header.html') ?>
+  <main>
+    <p class="text-4xl text-green-500">Hello world</p>
+  </main>
+  <?php include('../view/html/components/footer.html') ?>
+</body>
+
+</html>

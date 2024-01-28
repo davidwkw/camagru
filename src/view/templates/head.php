47084475395723e1a5cb626b1b0bf7c100f52571
Wang Khang Waei <devtesting404@gmail.com>
2024-01-28 22:58:38 +0800

    update: add head.php template"

---
+++ src/view/templates/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="global.css">
+    <?php
+    foreach ($cssPaths as $cssPath) {
+        echo "<link rel=\"stylesheet\" href=\"{$cssPath}\" />";
+    }
+
+    foreach ($scriptPaths as $scriptPath) {
+        echo "<script defer src=\"{$scriptPath}\"></script>";
+    }
+    ?>
+</head>

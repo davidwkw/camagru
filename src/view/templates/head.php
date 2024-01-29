456fb9e4662181b43fdcde1f5313ab3b7a25214b
Wang Khang Waei <devtesting404@gmail.com>
2024-01-29 13:23:12 +0800

    update: update navbar linking template

--- src/view/templates/head.php
+++ src/view/templates/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="global.css">
+    <!-- <link rel="stylesheet" href="global.css"> -->
     <?php
     foreach ($cssPaths as $cssPath) {
         echo "<link rel=\"stylesheet\" href=\"{$cssPath}\">";
